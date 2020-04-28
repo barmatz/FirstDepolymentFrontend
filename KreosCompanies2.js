bf99ded8f2f4daf354d946dd524d5d1948ac1c65
Blobfish-kate <k.shall0507@gmail.com>
2020-04-28 15:20:38 +0100

    Try to fix CORS error again

--- KreosCompanies2.js
+++ KreosCompanies2.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser")
+const cors = require("cors")
+
 const rp = require('request-promise');
 const url = 'http://www.kreoscapital.com/portfolio/';
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(cors())
 
 let INVESTMENTS;
 
 rp(url)
   .then(function(html){
-    //success!
     INVESTMENTS = JSON.parse(
     	html.substring(html.indexOf('INVESTMENTS') + 13, html.indexOf("</script><div class=") - 2)
     );  
@@ -25,7 +23,7 @@ rp(url)
   });
 
 app.get("/", function(req, res){
-	res.send(INVESTMENTS)
+	res.json(INVESTMENTS)
 })
 
 app.set('port', process.env.PORT || 3000);
